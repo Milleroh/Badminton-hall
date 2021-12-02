f25ef32634185569d7ca5a50422c1311aa5a19b5
Milleroh <58856131+Milleroh@users.noreply.github.com>
2021-12-02 18:04:15 +0800

    Update qc.py

--- qc.py
+++ qc.py
@@ -11,15 +11,15 @@ edge_options.add_argument('--disable-blink-features=AutomationControlled')
 
 driver = Edge(executable_path='MicrosoftWebDriver.exe', options=edge_options)
 start = time.clock()
+data = (datetime.datetime.now()+datetime.timedelta(days=2)).strftime("%Y-%m-%d")
+data = str(data)
+print(data)
+url = 'http://www.koksoft.com/weixinordernewv7.aspx?wxkey=153DC84BBF7CABBB49618F7ED48D5A310A30AB1DF70AD3A9D56E06C4361FE6E3A7257A467D56E248AB776CEFF445264D618B4CBC82AED3FDA1BF65E52E21239214900E6959D0178A6F43F133A18724422EE19A24CDE8D2929D141458590A907CFEC5A5357032FEE4B5677F755E27BBA9&lxbh=Y&orderdate='
+url = url + data
+driver.get(url)
 n = 0
 
 while(1):
-    data = (datetime.datetime.now()+datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    data = str(data)
-    print(data)
-    url = 'http://www.koksoft.com/weixinordernewv7.aspx?wxkey=153DC84BBF7CABBB49618F7ED48D5A310A30AB1DF70AD3A9D56E06C4361FE6E3A7257A467D56E248AB776CEFF445264D618B4CBC82AED3FDA1BF65E52E21239214900E6959D0178A6F43F133A18724422EE19A24CDE8D2929D141458590A907CFEC5A5357032FEE4B5677F755E27BBA9&lxbh=Y&orderdate='
-    url = url + data
-    driver.get(url)
     while(1):
         try:
             #8-9点主馆场地6为/html/body/div[1]/div/div[2]/div[2]/div[2]/div[1]/table/tbody/tr[15]/td[6]/span
@@ -27,9 +27,7 @@ while(1):
             break
         except:
             n += 1
-            if n > 5:
-                driver.refresh()
-                n = 0
+            print("error")
     try:
         button1.click()
     except:
